ff62159ca63a612aacefc3c26b6c50852b21c00f
mh105 <alexhe0218@gmail.com>
2024-11-21 16:46:57 -0800

    DOC: clean up empty spaces

--- SleepEEG_addpath.m
+++ SleepEEG_addpath.m
@@ -22,66 +22,66 @@ function [ dataDir, subject_list ] = SleepEEG_addpath(envpath)
 if strcmp(envpath(1:13), '/Applications')
     % ---------- for Alex local env -------------
     % set up directories
-    rootDir = '~/Dropbox/MIT_HST/NSRL_Purdon_Prerau/EEG';
-    
+    rootDir = '~/Dropbox/Active_projects/EEG';
+
     % Add path to Alex's code folder
     addpath([rootDir, '/code/sleepeeg_code'])
-    
+
     % Add path to ANT_interface_code folder
     addpath([rootDir, '/code/ANT_interface_code'])
-    
+
     % Add path to ANT importer
     addpath([rootDir, '/code/ANT_interface_code/ANTeepimport1.13'])
-    
+
     % Add path to EEGLAB
     addpath([rootDir, '/code/ANT_interface_code/eeglab14_1_2b'])
     eeglab nogui;
-    
+
     % Add path to EEGLAB firfilt plugin for pop_resample()
     addpath([rootDir, '/code/ANT_interface_code/eeglab14_1_2b/plugins/firfilt1.6.2'])
-    
+
     % Add path to helper functions
     addpath([rootDir, '/code/sleepeeg_code/helper_functions'])
     addpath([rootDir, '/code/sleepeeg_code/helper_functions/EDF_Deidentification_updated'])
-    
+
     % Add path to Data folder
     currentpath = pwd;
     cd([rootDir '/data'])
     dataDir = pwd; %.mex doesn't accept ~/ in path
     addpath(dataDir)
     cd(currentpath)
-    
+
 else
     % ---------- for workstation cerulean -------------
     % for the *** aeas *** project folder
-    
+
     % set up root directories
     rootDir = '/remote/projects/aeas';
-    
+
     % Add path to Alex's code folder
     addpath('/remote/users/mh1/code/sleepeeg_code')
-    
+
     % Add path to ANT_interface_code folder
     addpath('/remote/users/mh1/code/ant_interface_code')
-    
+
     % Add path to ANT importer
     addpath('/remote/users/mh1/code/ant_interface_code/ANTeepimport1.13')
-    
+
     % Add path to EEGLAB
     addpath('/remote/users/mh1/code/ant_interface_code/eeglab14_1_2b')
     eeglab nogui;
-    
+
     % Add path to EEGLAB firfilt plugin for pop_resample()
     addpath('/remote/users/mh1/code/ant_interface_code/eeglab14_1_2b/plugins/firfilt1.6.2')
-    
+
     % Add path to helper functions
     addpath('/remote/users/mh1/code/sleepeeg_code/helper_functions')
     addpath('/remote/users/mh1/code/sleepeeg_code/helper_functions/EDF_Deidentification_updated')
-    
+
     % Add path to Data folder
     dataDir = fullfile(rootDir, 'subject_data');
     addpath(dataDir)
-    
+
 end
 
 if nargout > 1
